51643be4c86fd51dfd9a4fd16defb4de6ba450b3
이종율 <ijong-yul@ijong-yul-ui-MacBookPro.local>
2024-05-11 20:45:25 +0900

    Create front-end js file

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`))
 
 const home = require("./src/routes/home");
 app.use("/",home); // use -> 미들웨어를 등록해주는 메서드.
