1963f6adb19958eafb4274b7b8bd5ce3bf14944f
이종율 <ijong-yul@ijong-yul-ui-MacBookPro.local>
2024-05-12 22:51:03 +0900

    login infomation Receiving login information from the web

--- app/app.js
+++ app/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const home = require("./src/routes/home");
 app.use("/",home); // use -> 미들웨어를 등록해주는 메서드.
